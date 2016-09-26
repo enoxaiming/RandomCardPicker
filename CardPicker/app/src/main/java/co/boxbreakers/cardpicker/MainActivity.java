540e1896e9991f5965d433a646a4ef8ed529141d
HaJunHyeok <pawn@boxbreakers.co>
2016-09-26 12:48:34 +0900

    Get Random image from gallery

--- CardPicker/app/src/main/java/co/boxbreakers/cardpicker/MainActivity.java
+++ CardPicker/app/src/main/java/co/boxbreakers/cardpicker/MainActivity.java
@@ -1,13 +1,65 @@
 package co.boxbreakers.cardpicker;
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    private ImageView mImageView;
+    private Bitmap currentBitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        String[] projection = new String[]{
+                MediaStore.Images.Media.DATA,
+        };
+
+        Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        Cursor cur = getContentResolver().query(images,
+                projection,
+                "",
+                null,
+                ""
+        );
+
+        final ArrayList<String> imagesPath = new ArrayList<String>();
+        if (cur.moveToFirst()) {
+
+            int dataColumn = cur.getColumnIndex(
+                    MediaStore.Images.Media.DATA);
+            do {
+                imagesPath.add(cur.getString(dataColumn));
+            } while (cur.moveToNext());
+        }
+        cur.close();
+        final Random random = new Random();
+        final int count = imagesPath.size();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int number = random.nextInt(count);
+                String path = imagesPath.get(number);
+                if (currentBitmap != null)
+                    currentBitmap.recycle();
+                currentBitmap = BitmapFactory.decodeFile(path);
+                mImageView.setImageBitmap(currentBitmap);
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
