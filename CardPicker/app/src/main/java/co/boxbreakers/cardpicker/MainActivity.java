63188929155339355d28d969fbe539a2a84d683d
HaJunHyeok <pawn@boxbreakers.co>
2016-11-13 02:05:07 +0900

    Nope

--- CardPicker/app/src/main/java/co/boxbreakers/cardpicker/MainActivity.java
+++ CardPicker/app/src/main/java/co/boxbreakers/cardpicker/MainActivity.java
@@ -1,18 +1,14 @@
 package co.boxbreakers.cardpicker;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
-import android.os.Handler;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewAnimator;
@@ -25,47 +21,76 @@ import co.boxbreakers.cardpicker.Animation.AnimationFactory;
 
 import static android.graphics.Bitmap.createScaledBitmap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView mImageView;
     private int i = 0;
+    private int t;
+    private int first,second;
     private BitmapFactory.Options options = new BitmapFactory.Options();
-    private Bitmap[] bitmap = new Bitmap[8];
-    private int imageList[] = {R.id.imageView1,R.id.imageView2,R.id.imageView3};
-    private int backlist[] = {R.id.back1,R.id.back2,R.id.back3};
-    private int viewFlipper[] = {R.id.viewFlipper1,R.id.viewFlipper2,R.id.viewFlipper3};
-
+    private Bitmap[] bitmap = new Bitmap[4];
+    private int imageList[] = {R.id.imageView1,R.id.imageView2,R.id.imageView3,R.id.imageView4,R.id.imageView5,R.id.imageView6,R.id.imageView7,R.id.imageView8};
+    private int backlist[] = {R.id.back1,R.id.back2,R.id.back3,R.id.back4,R.id.back5,R.id.back6,R.id.back7,R.id.back8};
+    private int viewFlipper[] = {R.id.viewFlipper1,R.id.viewFlipper2,R.id.viewFlipper3,R.id.viewFlipper4,R.id.viewFlipper5,R.id.viewFlipper6,R.id.viewFlipper7,R.id.viewFlipper8};
+    private ViewAnimator[] viewAnimator = new ViewAnimator[8];
+    private ViewAnimator vm1,vm2,vm3,vm4,vm5,vm6,vm7,vm8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getRandomBitmap();
+        int[] k = getRandomint();
+        System.out.println(k[0]+" "+k[1]+" " +k[2]);
+
         options.inSampleSize = 4;
         Bitmap bitmaps = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.back);
 
-        for (int v = 0 ; v < 3 ; v++) {
-            final ViewAnimator viewAnimator = (ViewAnimator)this.findViewById(viewFlipper[v]);
-            mImageView = (ImageView) findViewById(imageList[v]);
-            mImageView.setImageBitmap(bitmap[v]);
-            mImageView = (ImageView) findViewById(backlist[v]);
+        for(i =0; i<8; i++) {
+            viewAnimator[i] = (ViewAnimator)this.findViewById(viewFlipper[i]);
+            mImageView = (ImageView) findViewById(imageList[i]);
+            mImageView.setImageBitmap(bitmap[k[i]]);
+            mImageView = (ImageView) findViewById(backlist[i]);
             mImageView.setImageBitmap(bitmaps);
-            this.findViewById(imageList[v]).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AnimationFactory.flipTransition(viewAnimator, AnimationFactory.FlipDirection.LEFT_RIGHT);
-                }
-            });
-            this.findViewById(backlist[v]).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AnimationFactory.flipTransition(viewAnimator, AnimationFactory.FlipDirection.LEFT_RIGHT);
-                }
-            });
+            viewAnimator[i].setOnClickListener(this);
         }
 
+        vm1 = (ViewAnimator)this.findViewById(R.id.viewFlipper1);
+        vm2 = (ViewAnimator)this.findViewById(R.id.viewFlipper2);
+        vm3 = (ViewAnimator)this.findViewById(R.id.viewFlipper3);
+        vm4 = (ViewAnimator)this.findViewById(R.id.viewFlipper4);
+        vm5 = (ViewAnimator)this.findViewById(R.id.viewFlipper5);
+        vm6 = (ViewAnimator)this.findViewById(R.id.viewFlipper6);
+        vm7 = (ViewAnimator)this.findViewById(R.id.viewFlipper7);
+        vm8 = (ViewAnimator)this.findViewById(R.id.viewFlipper8);
 
+        vm1.setOnClickListener(this);
+        vm2.setOnClickListener(this);
+        vm3.setOnClickListener(this);
+        vm4.setOnClickListener(this);
+        vm5.setOnClickListener(this);
+        vm6.setOnClickListener(this);
+        vm7.setOnClickListener(this);
+        vm8.setOnClickListener(this);
     }
+
+    public int[] getRandomint() {
+        int[] a = new int[8];
+        Random random = new Random();
+        Random random1 = new Random();
+        for(int i = 0 ; i < 8 ; i+=2) {
+            a[i] = random.nextInt(4);
+        }
+        for(int j = 1 ; j < 8 ; j+=2) {
+            a[j] = random1.nextInt(4);
+        }
+        return a;
+    }
+
+    public void imageCheck() {
+        
+    }
+
 
     public Bitmap[] getRandomBitmap() {
         options.inSampleSize = 4;
@@ -100,5 +125,137 @@ public class MainActivity extends AppCompatActivity {
             }
         return bitmap;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.viewFlipper1:
+                AnimationFactory.flipTransition(vm1,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView1:
+                AnimationFactory.flipTransition(vm1,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back1:
+                AnimationFactory.flipTransition(vm1,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper2:
+                AnimationFactory.flipTransition(vm2,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView2:
+                AnimationFactory.flipTransition(vm2,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back2:
+                AnimationFactory.flipTransition(vm2,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+            case R.id.viewFlipper3:
+                AnimationFactory.flipTransition(vm3,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView3:
+                AnimationFactory.flipTransition(vm3,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back3:
+                AnimationFactory.flipTransition(vm3,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper4:
+                AnimationFactory.flipTransition(vm4,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView4:
+                AnimationFactory.flipTransition(vm4,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back4:
+                AnimationFactory.flipTransition(vm4,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper5:
+                AnimationFactory.flipTransition(vm5,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView5:
+                AnimationFactory.flipTransition(vm5,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back5:
+                AnimationFactory.flipTransition(vm5,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper6:
+                AnimationFactory.flipTransition(vm6,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView6:
+                AnimationFactory.flipTransition(vm6,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back6:
+                AnimationFactory.flipTransition(vm6,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper7:
+                AnimationFactory.flipTransition(vm7,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView7:
+                AnimationFactory.flipTransition(vm7,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back7:
+                AnimationFactory.flipTransition(vm7,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+
+
+            case R.id.viewFlipper8:
+                AnimationFactory.flipTransition(vm8,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+                break;
+
+            case R.id.imageView8:
+                AnimationFactory.flipTransition(vm8,
+                        AnimationFactory.FlipDirection.LEFT_RIGHT);
+
+                break;
+            case R.id.back8:
+                AnimationFactory.flipTransition(vm8,
+                        AnimationFactory.FlipDirection.RIGHT_LEFT);
+                break;
+        }
+    }
+
 
 }
